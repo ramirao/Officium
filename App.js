b5540a19d3acbfb54d4615700b5b3a750988973d
r4m1r40 <m.ramiro.costa@gmail.com>
2021-05-28 18:20:26 -0300

    primeiro commit graficos funcionando

--- App.js
+++ App.js
@@ -1,21 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
+
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Search from './screens/Search';
+import State from './screens/State';
+import Charts from './screens/Home';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
-export default function App() {
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Tab.Navigator
+      initialRouteName="Home"
+      tabCarOptions={{
+        activeTinColor: '#26872a',
+        inactiveTinColor: '#fff',
+        activeBackgroundColor: '#000000',
+        inactiveBackgroundColor: '#000000',
+          style: { 
+            backgroundColor: '#000000',
+            paddingBottom: 3
+          }
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={Charts}
+        options={{ 
+          tabBarLabel:'Home',
+          tabBarIcon: ({color, size}) => (
+            <MaterialCommunityIcons name="home-analytics" color={color} size={size} />
+          )
+        }}
+       />
+      <Tab.Screen 
+        name="Search" 
+        component={Search} 
+        options={{ 
+          tabBarLabel:'Search',
+          tabBarIcon: ({color, size}) => (
+            <MaterialCommunityIcons name="map-search" color={color} size={size} />
+          )
+        }}
+        />
+      <Tab.Screen 
+        name="State" 
+        component={State} 
+        options={{ 
+          tabBarLabel:'State',
+          tabBarIcon: ({color, size}) => (
+            <MaterialCommunityIcons name="rocket" color={color} size={size} />
+          )
+        }}
+        />
+    </Tab.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyTabs />
+    </NavigationContainer>
+  );
+}
---
+++ screens/State.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const State = () => {
+    return (
+        <View style={{ flex:1, justifyContent: 'center', alignItems: 'center'}}>
+            <Text>State</Text>
+        </View>
+    )
+}
+
+export default State
